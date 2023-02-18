5870e7345b5baf9a15b4f29de033564774907cfe
HARIS#10# <mdharishz107@gmail.com>
2023-02-18 22:12:08 +0530

    signup backend added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,35 @@
-const signup = async (req, res) => {};
+const bcrypt = require("bcrypt");
+const { UserModel } = require("../models/user.model");
+
+const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await UserModel.findOne({ email });
+
+  if (user) {
+    res.send({ msg: "User already exists. Please Login" });
+  }
+
+  bcrypt.hash(password, 6, async function (err, hash) {
+    if (err) {
+      res.send({ msg: "Something went wrong, please try again later" });
+    }
+
+    const user = new UserModel({
+      email,
+      name,
+      password: hash,
+      
+    });
+    try {
+      await user.save();
+      res.send({ msg: "Signup Successful" });
+    } catch (err) {
+      console.log(err);
+      res.send({ msg: "Something went wrong, please try again later" });
+    }
+  });
+};
 
 const login = async (req, res) => {};
 
--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  user_id: { type: Number },
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
