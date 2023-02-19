3348a252a6e4bdc7097beab530036f59b014cf0f
HARIS#10# <mdharishz107@gmail.com>
2023-02-19 12:25:37 +0530

    adding the login functionality in backend

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,8 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const { UserModel } = require("../models/user.model");
+require("dotenv").config();
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -19,7 +22,6 @@ const signup = async (req, res) => {
       email,
       name,
       password: hash,
-      
     });
     try {
       await user.save();
@@ -31,7 +33,25 @@ const signup = async (req, res) => {
   });
 };
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email });
+  const hash = user.password;
+
+  bcrypt.compare(password, hash, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.send({ msg: "Something went wrong, Please try again " });
+    }
+    if (result) {
+      const token = jwt.sign({ userId: user._Id }, process.env.PRIVATE_KEY);
+      res.send({ msg: "Login Successful", token });
+    } else {
+      res.send({ msg: "Invalid credentials, Please Signup if you haven't" });
+    }
+  });
+};
 
 module.exports = {
   signup,
