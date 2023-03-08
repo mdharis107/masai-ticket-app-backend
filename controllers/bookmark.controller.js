85deef843eb9d8ae718d884fbbb27a003c8efef2
HARIS#10# <mdharishz107@gmail.com>
2023-03-08 13:06:01 +0530

    posting bookmark created

---
+++ controllers/bookmark.controller.js
@@ -0,0 +1,23 @@
+const { BookmarkModel } = require("../models/bookmark.model");
+
+const getBookmark = () => {};
+
+const postBookmark = async (req, res) => {
+  const { title, category, message, userId } = req.body;
+
+  const ticket = new BookmarkModel({
+    title,
+    category,
+    message,
+    userId,
+  });
+  try {
+    await ticket.save();
+    res.status(201).send({ msg: "Ticket has been Bookmarked" });
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: "Something went wrong" });
+  }
+};
+
+module.exports + { postBookmark, getBookmark };
